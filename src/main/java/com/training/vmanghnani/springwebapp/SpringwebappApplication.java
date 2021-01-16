6152249c45137a0bc3f16fb280b305d92cdbc11c
Vishal Manghnani <vishal-lollers@hotmail.com>
2021-01-16 16:50:25 +0800

    Initial Project

---
+++ src/main/java/com/training/vmanghnani/springwebapp/SpringwebappApplication.java
@@ -0,0 +1,13 @@
+package com.training.vmanghnani.springwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/training/vmanghnani/springwebapp/SpringwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.vmanghnani.springwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
